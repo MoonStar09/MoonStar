75b94903aed258f811a18d70f299fe95b826ad5a
MoonStar09 <aliyimraan17@gmail.com>
2026-09-16 14:43:19 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "Custom Status", // required by Discord API, not shown in the client
         state: "Watching Server 👀 ",     // this is what people actually see
-        type: 1,               // Custom
+        type: 4,               // Custom
       },
     ],
   },
